3b99ccf162e5602f35567b6e81d255bdc06dec92
Yavuz <yavuz.orbey@gmail.com>
2019-09-25 15:27:01 -0400

    fix migrations

--- database/migrations/2019_05_12_170756_create_section_book_question_table.php
+++ database/migrations/2019_05_12_170756_create_section_book_question_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSectionQuestionTable extends Migration
+class CreateSectionBookQuestionTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2019_06_25_225238_create_section_book_question_user_table.php
+++ database/migrations/2019_06_25_225238_create_section_book_question_user_table.php
@@ -13,13 +13,13 @@ class CreateSectionQuestionUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('section_question_user', function (Blueprint $table) {
+        Schema::create('section_book_question_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('section_question_id');
+            $table->unsignedInteger('section_book_question_id');
             $table->unsignedInteger('user_id');
             $table->timestamps();
 
-            $table->foreign('section_question_id')->references('id')->on('section_questions')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('section_book_question_id')->references('id')->on('section_book_questions')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
